438ca10e6954af7566dd4de3a9d753bca4c33f91
MariaGF <miriangimenez@gmail.com>
2023-01-09 02:26:25 +0100

    cambios formulario

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 let form = document.getElementById('formTransaction');
 
 form.addEventListener('submit', function(event){
-    console.log(event);
-  event.preventDefault();
-}
+  event.preventDefault();  
+  let transactionFormData = new FormData(form);
 
-)
+  let tableTransaction = document.getElementById('tableTransaction');
+  let newTransactionRow = tableTransaction.insertRow(-1);
+
+  let newTypeCell =  newTransactionRow.insertCell(0);
+  newTypeCell.textContent = transactionFormData.get('');
+
+  newTypeCell = newTransactionRow.insertCell(1);
+  newTypeCell.textContent = transactionFormData.get('Name');
+
+  newTypeCell = newTransactionRow.insertCell(2);
+  newTypeCell.textContent = transactionFormData.get('SurName');
+
+  newTypeCell = newTransactionRow.insertCell(3);
+  newTypeCell.textContent = transactionFormData.get('SecondSurName');
+
+
+})
+
+
